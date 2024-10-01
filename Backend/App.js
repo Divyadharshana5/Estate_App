c0dc13b7430f127843cf9b73e6ba0f8166483cda
divyadharshana5 <divyadharshana3@gmail.com>
2024-10-01 18:17:19 +0530

    Initialize the route

--- Backend/App.js
+++ Backend/App.js
@@ -6,6 +6,30 @@ app.use("/Backend/test", (req, res) => {
   res.send("It works!");
 });
 
+app.use("/Backend/auth/register", (req, res) => {
+  res.send("It works!");
+});
+
+app.use("/Backend/auth/login", (req, res) => {
+  res.send("It works!");
+});
+
+app.use("/Backend/auth/logout", (req, res) => {
+  res.send("It works!");
+});
+
+app.use("/Backend/posts", (req, res) => {
+  res.send("It works!");
+});
+
+app.use("/Backend/posts", (req, res) => {
+  res.send("It works!");
+});
+
+app.use("/Backend/posts/12312", (req, res) => {
+  res.send("It works!");
+});
+
 app.listen(8800, () => {
   console.log("Server is running!");
 });
