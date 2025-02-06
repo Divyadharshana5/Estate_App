195e4132e96292a5532603f252d0d329ade22ed1
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-02-06 15:08:25 +0530

    import prisma/client

--- Backend/Controllers/auth.controller.js
+++ Backend/Controllers/auth.controller.js
@@ -8,6 +8,7 @@ export const register = async (req, res) => {
   const hashedPassword = await bcrypt.hash(password, 10);
   console.log(hashedPassword);
   //CREATE A NEW USER AND SAVE TO DB
+  const newUser=await
 };
 
 export const login = (req, res) => {};
