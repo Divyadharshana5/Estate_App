d2e0e701fdc0ee25baee30f5b2887f5f80a0a92c
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-02-07 15:50:15 +0530

    install cookieParser

--- Backend/App.js
+++ Backend/App.js
@@ -1,41 +1,15 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/Backend/posts", postRoute);
 app.use("/Backend/auth", authRoute);
-
-// app.use("/Backend/test", (req, res) => {
-//   res.send("It works!");
-// });
-
-// app.use("/Backend/auth/register", (req, res) => {
-//   res.send("It works!");
-// });
-
-// app.use("/Backend/auth/login", (req, res) => {
-//   res.send("It works!");
-// });
-
-// app.use("/Backend/auth/logout", (req, res) => {
-//   res.send("It works!");
-// });
-
-// app.use("/Backend/posts", (req, res) => {
-//   res.send("It works!");
-// });
-
-// app.use("/Backend/posts", (req, res) => {
-//   res.send("It works!");
-// });
-
-// app.use("/Backend/posts/12312", (req, res) => {
-//   res.send("It works!");
-// });
 
 app.listen(8000, () => {
   console.log("Server is running!");
--- Backend/Controllers/auth.controller.js
+++ Backend/Controllers/auth.controller.js
@@ -22,6 +22,25 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {};
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+    if (!user) return res.status(401).json({ message: "Invalid Credentials" });
+
+    const isPasswordValid = await bcrypt.compareSync(password, user.password);
+
+    if (!isPasswordValid)
+      return res.status(401).json({ message: "Invalid Credentials" });
+
+    res.setHeader("Set-Cookie", "test=" + "myValue").json("Success");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to login!" });
+  }
+};
 
 export const logout = (req, res) => {};
