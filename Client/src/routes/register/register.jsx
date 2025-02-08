5630ba2ff56f40655ddd78bfe16ed0492daed43d
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-02-08 18:45:45 +0530

    updated client

---
+++ Client/src/routes/register/register.jsx
@@ -0,0 +1,53 @@
+import "./register.scss";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+
+function Register() {
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    try {
+      const res = await axios.post(
+        "http://localhost:8000/Backend/auth/register",
+        {
+          username,
+          email,
+          password,
+        }
+      );
+      navigate("/login");
+    } catch (err) {
+      setError(err.response.data.message);
+    }
+  };
+  return (
+    <div className="register">
+      <div className="formContainer">
+        <form onSubmit={handleSubmit}>
+          <h1>Create an Account</h1>
+          <input name="username" type="text" placeholder="Username" />
+          <input name="email" type="text" placeholder="Email" />
+          <input name="password" type="password" placeholder="Password" />
+          <button>Register</button>
+          {error && <span>{error}</span>}
+          <Link to="/login">Already have an account?</Link>
+        </form>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default Register;
