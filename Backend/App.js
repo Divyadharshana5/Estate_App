4aa4df9f8c0fcd45e76f01b4a8404a8123538fbb
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-02-08 16:00:48 +0530

    install cors

--- Backend/App.js
+++ Backend/App.js
@@ -6,7 +6,7 @@ import postRoute from "./routes/post.route.js";
 
 const app = express();
 
-app.use(cors({origin:}))
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
