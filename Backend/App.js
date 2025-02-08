9ad1da0931b7d9416fce76273e8c03810de70d3b
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-02-08 15:53:14 +0530

    install axios

--- Backend/App.js
+++ Backend/App.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 
 const app = express();
 
+app.use(cors({origin:}))
 app.use(express.json());
 app.use(cookieParser());
 
