57de52f345fef7bb380dc04696b40c72d750626c
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-02-08 19:37:10 +0530

    updated login page

--- Client/src/routes/register/register.jsx
+++ Client/src/routes/register/register.jsx
@@ -2,14 +2,17 @@ import "./register.scss";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
+import apiRequest from "../../lib/apiRequest";
 
 function Register() {
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsLoading(true);
     const formData = new FormData(e.target);
 
     const username = formData.get("username");
@@ -17,17 +20,16 @@ function Register() {
     const password = formData.get("password");
 
     try {
-      const res = await axios.post(
-        "http://localhost:8000/Backend/auth/register",
-        {
-          username,
-          email,
-          password,
-        }
-      );
+      const res = await apiRequest.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
       navigate("/login");
     } catch (err) {
       setError(err.response.data.message);
+    } finally {
+      setIsLoading(false);
     }
   };
   return (
@@ -38,7 +40,7 @@ function Register() {
           <input name="username" type="text" placeholder="Username" />
           <input name="email" type="text" placeholder="Email" />
           <input name="password" type="password" placeholder="Password" />
-          <button>Register</button>
+          <button disabled={isLoading}>Register</button>
           {error && <span>{error}</span>}
           <Link to="/login">Already have an account?</Link>
         </form>
