2f1f6c8ee5dbfe59d0236730576a27942dc0f981
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-02-09 14:54:59 +0530

    update loginpage

--- Client/src/routes/login/login.jsx
+++ Client/src/routes/login/login.jsx
@@ -1,12 +1,14 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import "./login.scss";
 import { Link, useNavigate } from "react-router-dom";
 import apiRequest from "../../lib/apiRequest";
+import { AuthContext } from "../../Context/AuthContext";
 
 function Login() {
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
+  const { updateUser } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
@@ -24,7 +26,7 @@ function Login() {
         username,
         password,
       });
-      localStorage.setItem("user", JSON.stringify(res.data));
+      updateUser(await res.data);
 
       navigate("/");
     } catch (err) {
