da9fd8676b2fc8c44c6ee1b2a75d471bc26140e5
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-06-14 19:18:03 +0530

    update post.route.js

--- Backend/routes/post.route.js
+++ Backend/routes/post.route.js
@@ -1,12 +1,19 @@
 import express from "express";
 import { verifyToken } from "../middleware/verifyToken.js";
+import {
+  addPost,
+  deletePost,
+  getPost,
+  getPosts,
+  updatePost,
+} from "../Controllers/post.controller.js";
 
 const router = express.Router();
 
 router.get("/", getPosts);
 router.get("/:id", getPost);
-router.get("/", verifyToken, addPosts);
-router.get("/:id", verifyToken, updatePosts);
-router.get("/:id", verifyToken, deletePosts);
+router.get("/", verifyToken, addPost);
+router.get("/:id", verifyToken, updatePost);
+router.get("/:id", verifyToken, deletePost);
 
 export default router;
