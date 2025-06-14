47a32dc0a953c2cbe763734198277a34473ad354
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-06-14 19:04:58 +0530

    update post.route.js

--- Backend/routes/post.route.js
+++ Backend/routes/post.route.js
@@ -2,4 +2,10 @@ import express from "express";
 
 const router = express.Router();
 
+router.get("/", getPosts);
+router.get("/:id", getPost);
+router.get("/", verifyToken, addPosts);
+router.get("/:id", verifyToken, updatePosts);
+router.get("/:id", verifyToken, deletePosts);
+
 export default router;
