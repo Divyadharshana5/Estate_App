f3ea592a07640149d4b6631140314e68668d8330
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-06-14 19:26:34 +0530

    update post.controller.js

--- Backend/Controllers/post.controller.js
+++ Backend/Controllers/post.controller.js
@@ -3,7 +3,7 @@ import prisma from "../lib/prisma.js";
 export const getPosts = async (req, res) => {
   try {
     const posts = await prisma.post.findMany();
-    res.status(200).json();
+    res.status(200).json(posts);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to get posts" });
