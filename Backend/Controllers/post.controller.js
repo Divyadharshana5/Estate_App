bc79295140c03ef354fa40b48905ee91562d136d
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-06-14 19:14:48 +0530

    update post.controller.js

--- Backend/Controllers/post.controller.js
+++ Backend/Controllers/post.controller.js
@@ -18,12 +18,12 @@ export const getPost = async (req, res) => {
   }
 };
 
-export const getPosts = async (req, res) => {
+export const addPost = async (req, res) => {
   try {
     res.status(200).json();
   } catch (err) {
     console.log(err);
-    res.status(500).json({ message: "Failed to get posts" });
+    res.status(500).json({ message: "Failed to create post" });
   }
 };
 
