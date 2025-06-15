4166fd1870fd475b664b0e04670761fc770a66d9
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-06-15 12:16:37 +0530

    update newPostPage.jsx

--- Client/src/routes/newPostPage/newPostPage.jsx
+++ Client/src/routes/newPostPage/newPostPage.jsx
@@ -12,6 +12,7 @@ function NewPostPage() {
               <label htmlFor="title">Title</label>
               <input id="title" name="title" type="text" />
             </div>
+            <ReactQuill />
             <div className="item">
               <label htmlFor="price">Price</label>
               <input id="price" name="price" type="number" />
