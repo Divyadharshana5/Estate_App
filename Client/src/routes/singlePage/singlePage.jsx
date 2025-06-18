49597e1bd7f89589ecef9a991a4c0151d1b46b65
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-06-18 18:02:58 +0530

    update singlePage.jsx

--- Client/src/routes/singlePage/singlePage.jsx
+++ Client/src/routes/singlePage/singlePage.jsx
@@ -50,7 +50,11 @@ function SinglePage() {
               <img src="/pet.png" alt="" />
               <div className="featureText">
                 <span>Pet Policy</span>
-                <p>Pets Allowed</p>
+                {post.postDetail.pet === "allowed" ? (
+                  <p>Pets Allowed</p>
+                ) : (
+                  <p>Pets not allowed</p>
+                )}
               </div>
             </div>
             <div className="feature">
