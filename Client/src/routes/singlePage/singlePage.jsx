ce64d7f37f36c4dfa922ed805e764865fd16dadf
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-06-18 18:14:15 +0530

    update singlePage.jsx

--- Client/src/routes/singlePage/singlePage.jsx
+++ Client/src/routes/singlePage/singlePage.jsx
@@ -3,6 +3,7 @@ import Slider from "../../components/slider/Slider";
 import Map from "../../components/map/Map";
 import { singlePostData, userData } from "../../lib/dummydata";
 import { useLoaderData } from "react-router-dom";
+import DOMPurify from "dompurify";
 
 function SinglePage() {
   const post = useLoaderData();
@@ -27,7 +28,12 @@ function SinglePage() {
                 <span>{post.user.username}</span>
               </div>
             </div>
-            <div className="bottom">{post.postDetail.desc}</div>
+            <div
+              className="bottom"
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(post.postDetail.desc),
+              }}
+            ></div>
           </div>
         </div>
       </div>
