23577f91b7062d3b1120e4701a88f42478426850
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-06-18 17:38:08 +0530

    update singlePage.jsx

--- Client/src/routes/singlePage/singlePage.jsx
+++ Client/src/routes/singlePage/singlePage.jsx
@@ -6,28 +6,28 @@ import { useLoaderData } from "react-router-dom";
 
 function SinglePage() {
   const post = useLoaderData();
-  console.log(post);
+
   return (
     <div className="singlePage">
       <div className="details">
         <div className="wrapper">
-          <Slider images={singlePostData.images} />
+          <Slider images={post.images} />
           <div className="info">
             <div className="top">
               <div className="post">
-                <h1>{singlePostData.title}</h1>
+                <h1>{post.title}</h1>
                 <div className="address">
                   <img src="/pin.png" alt="" />
-                  <span>{singlePostData.address}</span>
+                  <span>{post.address}</span>
                 </div>
-                <div className="price">$ {singlePostData.price}</div>
+                <div className="price">$ {post.price}</div>
               </div>
               <div className="user">
                 <img src={userData.img} alt="" />
                 <span>{userData.name}</span>
               </div>
             </div>
-            <div className="bottom">{singlePostData.description}</div>
+            <div className="bottom">{post.description}</div>
           </div>
         </div>
       </div>
