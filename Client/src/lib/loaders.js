6f93883676904c0ba153ae19fc24db354704f6ce
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-06-19 12:35:42 +0530

    update loaders.js

--- Client/src/lib/loaders.js
+++ Client/src/lib/loaders.js
@@ -7,6 +7,6 @@ export const singlePageLoader = async ({ request, params }) => {
 
 export const listPageLoader = async ({ request, params }) => {
   const query = request.url.split("?")[1];
-  const res = await apiRequest("/posts?" + params.id);
+  const res = await apiRequest("/posts?" + query);
   return res.data;
 };
