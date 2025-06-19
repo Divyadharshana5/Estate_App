c58d1b2c9385c32609f8119694d6a8dc43f0d1d1
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-06-19 12:39:56 +0530

    update post.controller.js

--- Backend/Controllers/post.controller.js
+++ Backend/Controllers/post.controller.js
@@ -2,9 +2,15 @@ import prisma from "../lib/prisma.js";
 
 export const getPosts = async (req, res) => {
   const query = req.query;
-  console.log(query);
+
   try {
-    const posts = await prisma.post.findMany();
+    const posts = await prisma.post.findMany({
+      where: {
+        city: query.city || undefined,
+        type: query.type || undefined,
+        property: query.property || undefined,
+      },
+    });
     res.status(200).json(posts);
   } catch (err) {
     console.log(err);
