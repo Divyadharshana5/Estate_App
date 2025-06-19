bacd1862b70b8f6310be48da436830c9fc0b7597
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-06-19 12:30:11 +0530

    update Map.jsx

--- Client/src/components/map/Map.jsx
+++ Client/src/components/map/Map.jsx
@@ -6,7 +6,11 @@ import Pin from "../pin/Pin";
 function Map({ items }) {
   return (
     <MapContainer
-      center={items.length === 1}
+      center={
+        items.length === 1
+          ? [items[0].latitude, items[0].longitude]
+          : [52.4797, -1.90269]
+      }
       zoom={7}
       scrollWheelZoom={false}
       className="map"
