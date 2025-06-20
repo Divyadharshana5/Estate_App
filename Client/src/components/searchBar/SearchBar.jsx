99531394f5d531cb3353bee7df797f5f4a65f8fd
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-06-20 18:05:11 +0530

    update SearchBar.jsx

--- Client/src/components/searchBar/SearchBar.jsx
+++ Client/src/components/searchBar/SearchBar.jsx
@@ -56,7 +56,7 @@ function SearchBar() {
           onChange={handleChange}
         />
         <Link
-          to={`/list?type=${query.type}&city=${query.city}&minPrice=${query.minPrice}`}
+          to={`/list?type=${query.type}&city=${query.city}&minPrice=${query.minPrice}&maxPrice=${query.maxPrice}`}
         >
           <button>
             <img src="/search.png" alt="" />
