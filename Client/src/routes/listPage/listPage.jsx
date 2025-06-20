09288cf62ffdc182b118d570cfd1c88c54a2132d
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-06-20 17:54:10 +0530

    update listPage.jsx

--- Client/src/routes/listPage/listPage.jsx
+++ Client/src/routes/listPage/listPage.jsx
@@ -1,25 +1,27 @@
-import { listData } from "../../lib/dummydata";
 import "./listPage.scss";
-import Filter from "../../components/filter/Filter"
-import Card from "../../components/card/Card"
+import Filter from "../../components/filter/Filter";
+import Card from "../../components/card/Card";
 import Map from "../../components/map/Map";
+import { useLoaderData } from "react-router-dom";
 
 function ListPage() {
-  const data = listData;
+  const posts = useLoaderData();
 
-  return <div className="listPage">
-    <div className="listContainer">
-      <div className="wrapper">
-        <Filter/>
-        {data.map(item=>(
-          <Card key={item.id} item={item}/>
-        ))}
+  return (
+    <div className="listPage">
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          {posts.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+      <div className="mapContainer">
+        <Map items={posts} />
       </div>
     </div>
-    <div className="mapContainer">
-      <Map items={data}/>
-    </div>
-  </div>;
+  );
 }
 
 export default ListPage;
