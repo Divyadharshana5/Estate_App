f11482ea598507997a3372c3bbdf304b60eac460
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-06-25 14:05:14 +0530

    update user.route.js

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -4,6 +4,7 @@ import {
   getUser,
   getUsers,
   updateUser,
+  savePost,
 } from "../Controllers/user.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
@@ -12,7 +13,7 @@ const router = express.Router();
 router.get("/", getUsers);
 router.get("/:id", verifyToken, getUser);
 router.put("/:id", verifyToken, updateUser);
-
 router.delete("/:id", verifyToken, deleteUser);
+router.post("/save", verifyToken, savePost);
 
 export default router;
