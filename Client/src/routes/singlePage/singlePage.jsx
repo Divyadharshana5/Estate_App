2f5f5502a47450b73fb1440ae63c09d6f6d8e4a4
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-06-27 13:54:14 +0530

    update singlePage.jsx

--- Client/src/routes/singlePage/singlePage.jsx
+++ Client/src/routes/singlePage/singlePage.jsx
@@ -1,12 +1,13 @@
 import "./singlePage.scss";
 import Slider from "../../components/slider/Slider";
 import Map from "../../components/map/Map";
+import { singlePostData, userData } from "../../lib/dummydata";
 import { useLoaderData } from "react-router-dom";
 import DOMPurify from "dompurify";
 
 function SinglePage() {
   const post = useLoaderData();
-
+  console.log(post);
   return (
     <div className="singlePage">
       <div className="details">
