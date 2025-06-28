b8b89c63c142ce5c57defd49debfe6aef953b710
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-06-28 12:10:22 +0530

    update user.route.js

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -15,5 +15,6 @@ router.get("/:id", verifyToken, getUser);
 router.put("/:id", verifyToken, updateUser);
 router.delete("/:id", verifyToken, deleteUser);
 router.post("/save", verifyToken, savePost);
+router.get("/profilePosts", verifyToken, profilePosts);
 
 export default router;
