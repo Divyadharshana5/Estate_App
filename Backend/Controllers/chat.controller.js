6a2a8ee250655468ec4f50edda21fe7f6f0b529e
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-01 13:20:15 +0530

    update chat.controller.js

--- Backend/Controllers/chat.controller.js
+++ Backend/Controllers/chat.controller.js
@@ -8,3 +8,21 @@ export const getChats = async (req, res) => {
     res.status(500).json({ message: "Failed to get chats!" });
   }
 };
+
+export const getChats = async (req, res) => {
+  try {
+    res.status(200).json(users);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to get chats!" });
+  }
+};
+
+export const getChats = async (req, res) => {
+  try {
+    res.status(200).json(users);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to get chats!" });
+  }
+};
