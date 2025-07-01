dce1ffe445777b8c5b7e9b51e69bc1dbee32aa1b
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-01 13:30:23 +0530

    update message.route.js

--- Backend/routes/message.route.js
+++ Backend/routes/message.route.js
@@ -1,10 +1,9 @@
 import express from "express";
-import { addMessage } from "../Controllers/user.controller.js";
+import { addMessage } from "../Controllers/message.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-router.post("/save", verifyToken, savePost);
-router.get("/profilePosts", verifyToken, profilePosts);
+router.post("/save", verifyToken, addMessage);
 
 export default router;
