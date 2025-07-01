c9aba735714ab31d47e20ecb6ba27aee92c8151c
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-01 13:24:17 +0530

    update chat.controller.js

--- Backend/Controllers/chat.controller.js
+++ Backend/Controllers/chat.controller.js
@@ -3,7 +3,11 @@ import prisma from "../lib/prisma.js";
 export const getChats = async (req, res) => {
   const tokenUserId = req.userId;
   try {
-    const chats = await prisma.chat.findMany();
+    const chats = await prisma.chat.findMany({
+      where: {
+        userIds: {},
+      },
+    });
     res.status(200).json(users);
   } catch (err) {
     console.log(err);
