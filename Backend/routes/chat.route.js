cd5adda1c44578e2a4cb2201b8a8ef54ed81ff24
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-01 13:09:01 +0530

    update chat.route.js

--- Backend/routes/chat.route.js
+++ Backend/routes/chat.route.js
@@ -5,6 +5,7 @@ import { verifyToken } from "../middleware/verifyToken.js";
 const router = express.Router();
 
 router.get("/search/:id", verifyToken, getchats);
+router.get("/search/:id", verifyToken, getchat);
 router.put("/:id", verifyToken, updateUser);
 router.delete("/:id", verifyToken, deleteUser);
 router.post("/save", verifyToken, savePost);
