29fdee89f5096024b047a57da93ec3afaa2b846a
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-01 12:31:45 +0530

    update List.jsx

--- Client/src/components/list/List.jsx
+++ Client/src/components/list/List.jsx
@@ -1,15 +1,14 @@
-import './list.scss'
-import Card from"../card/Card"
-import {listData} from"../../lib/dummydata"
+import "./list.scss";
+import Card from "../card/Card";
 
-function List(){
+function List({ posts }) {
   return (
-    <div className='list'>
-      {listData.map(item=>(
-        <Card key={item.id} item={item}/>
+    <div className="list">
+      {posts.map((item) => (
+        <Card key={item.id} item={item} />
       ))}
     </div>
-  )
+  );
 }
 
-export default List
+export default List;
