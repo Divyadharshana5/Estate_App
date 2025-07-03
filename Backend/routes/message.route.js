fd99b6360a2241f21c114fae21b3c6b0c38cccc1
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-03 10:52:41 +0530

    update message.route.js

--- Backend/routes/message.route.js
+++ Backend/routes/message.route.js
@@ -4,6 +4,6 @@ import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-router.post("/save", verifyToken, addMessage);
+router.post("/:chatId", verifyToken, addMessage);
 
 export default router;
