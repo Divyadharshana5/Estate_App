b72fadc80e4fb770bf1b4f9459e9adb613aa3c51
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-03 10:50:17 +0530

    update message.controller.js

--- Backend/Controllers/message.controller.js
+++ Backend/Controllers/message.controller.js
@@ -1,6 +1,7 @@
 import prisma from "../lib/prisma.js";
 
 export const addMessage = async (req, res) => {
+  const tokenUserId = req.userId;
   try {
     res.status(200).json(users);
   } catch (err) {
