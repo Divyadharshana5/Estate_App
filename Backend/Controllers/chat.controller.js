bcee9beedc98b9059470a2017dd415b564647970
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-03 10:47:13 +0530

    update chat.controller.js

--- Backend/Controllers/chat.controller.js
+++ Backend/Controllers/chat.controller.js
@@ -78,6 +78,12 @@ export const readChat = async (req, res) => {
           hasSome: [tokenUserId],
         },
       },
+
+      data: {
+        seenBy: {
+          set: [tokenUserId],
+        },
+      },
     });
     res.status(200).json(users);
   } catch (err) {
