9846742122a555f543bc1c753eee79fe4e284f6d
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-04 10:55:11 +0530

    update message.controller.js

--- Backend/Controllers/message.controller.js
+++ Backend/Controllers/message.controller.js
@@ -34,7 +34,7 @@ export const addMessage = async (req, res) => {
         lastMessage: text,
       },
     });
-    res.status(200).json(users);
+    res.status(200).json(message);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Failed to add messages!" });
