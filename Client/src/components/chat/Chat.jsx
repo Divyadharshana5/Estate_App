095a0cad82c8f7e7ff643c6565358d1ea72059fb
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-04 11:03:34 +0530

    update chat.jsx

--- Client/src/components/chat/Chat.jsx
+++ Client/src/components/chat/Chat.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./chat.scss";
 
-function Chat() {
+function Chat({ chats }) {
   const [chat, setChat] = useState(true);
   return (
     <div className="chat">
@@ -66,7 +66,9 @@ function Chat() {
               />
               John Doe
             </div>
-            <span className="close" onClick={()=>setChat(null)}>X</span>
+            <span className="close" onClick={() => setChat(null)}>
+              X
+            </span>
           </div>
           <div className="center">
             <div className="chatMessage">
