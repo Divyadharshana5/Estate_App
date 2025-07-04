4fe039d2c93ecaf37c4846774b865f987d7b06e5
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-04 10:58:53 +0530

    update loaders.js

--- Client/src/lib/loaders.js
+++ Client/src/lib/loaders.js
@@ -16,6 +16,8 @@ export const listPageLoader = async ({ request, params }) => {
 
 export const profilePageLoader = async () => {
   const postPromise = apiRequest("/users/profilePosts");
+  const chatPromise = apiRequest("/chats");
+
   return defer({
     postResponse: postPromise,
   });
