81510f4f78afa21a92a2eb877ce1429122deb894
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-07 13:41:48 +0530

    Update Chat.jsx

--- Client/src/components/chat/Chat.jsx
+++ Client/src/components/chat/Chat.jsx
@@ -6,15 +6,13 @@ function Chat({ chats }) {
     <div className="chat">
       <div className="messages">
         <h1>Messages</h1>
-
-        <div className="message">
-          <img
-            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-          />
-          <span>John Doe</span>
-          <p>Lorem ipsum dolor sit amet...</p>
-        </div>
+        {chats.map((c) => (
+          <div className="message" key={c.id}>
+            <img src={chat.receiver.avatar || "/noavatar."} alt="" />
+            <span>John Doe</span>
+            <p>Lorem ipsum dolor sit amet...</p>
+          </div>
+        ))}
       </div>
       {chat && (
         <div className="chatBox">
