2b59cb433761c7537a852c4e5b50439526749e85
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-09 12:05:01 +0530

    Update Chat.jsx

--- Client/src/components/chat/Chat.jsx
+++ Client/src/components/chat/Chat.jsx
@@ -25,6 +25,7 @@ function Chat({ chats }) {
     if (!text) return;
     try {
       const res = await apiRequest.post("/messages/" + chat.id, { text });
+      setChat((prev) => ({ ...prev, messages: [...prev.messages] }));
     } catch (err) {
       console.log(err);
     }
