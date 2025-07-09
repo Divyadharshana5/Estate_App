60bfecb5c4f9d2812f54afd7193f41dabc621455
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-09 12:23:46 +0530

    Update SocketContext.jsx

---
+++ Client/src/Context/SocketContext.jsx
@@ -0,0 +1,34 @@
+import { createContext, useEffect, useState } from "react";
+import { io } from "socket.io-client";
+
+export const SocketContext = createContext();
+
+export const SocketContextProvider = ({ children }) => {
+  const [socket, setSocket] = useState(() => {
+    try {
+      const storedUser = localStorage.getItem("user");
+      return storedUser ? JSON.parse(storedUser) : null;
+    } catch (error) {
+      console.error("Error parsing JSON from localStorage:", error);
+      return null;
+    }
+  });
+
+  const updateUser = (data) => {
+    setCurrentUser(data);
+  };
+
+  useEffect(() => {
+    if (currentUser !== null) {
+      localStorage.setItem("user", JSON.stringify(currentUser));
+    } else {
+      localStorage.removeItem("user");
+    }
+  }, [currentUser]);
+
+  return (
+    <SocketContext.Provider value={{ currentUser, updateUser }}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
