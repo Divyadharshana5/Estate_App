e2ec5238587879285fdd4fcfafc43935805c4808
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-10 05:59:11 +0530

    update main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
 import { AuthContextProvider } from "./Context/AuthContext.jsx";
+import { SocketContext } from "./Context/SocketContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthContextProvider>
-      <App />
+      <SocketContext>
+        <App />
+      </SocketContext>
     </AuthContextProvider>
   </React.StrictMode>
 );
