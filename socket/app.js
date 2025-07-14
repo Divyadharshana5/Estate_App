a119e181d91e124c01463b21b5c4a6a8961a6ac4
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-14 10:42:40 +0530

    updated app.js

--- socket/app.js
+++ socket/app.js
@@ -31,7 +31,7 @@ io.on("connection", (socket) => {
 
 socket.on("sendMessage", ({ receiverId, data }) => {
   const receiver = getUser(receiverId);
-  io.to(receiver.socketId).emit("getMessage"), {};
+  io.to(receiver.socketId).emit("getMessage", data);
 });
 
 socket.on("disconnect", () => {
