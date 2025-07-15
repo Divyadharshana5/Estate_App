840283cbca163ff458c386c0f9056024f84c9867
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-15 10:59:45 +0530

    updated SocketContext.jsx

--- Client/src/Context/SocketContext.jsx
+++ Client/src/Context/SocketContext.jsx
@@ -4,13 +4,16 @@ import { io } from "socket.io-client";
 export const SocketContext = createContext();
 
 export const SocketContextProvider = ({ children }) => {
+  const { currentUser } = useContext(AuthContext);
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
     setSocket(io("http://localhost:4000"));
   }, []);
 
-  useState(() => {}, []);
+  useState(() => {
+    currentUser && socket?.emit("");
+  }, [currentUser]);
 
   return (
     <SocketContext.Provider value={{ socket }}>
