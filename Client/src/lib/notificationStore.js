fb656dcf5b9ebf350bbb419cb6e95e731dc69ba2
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-15 11:19:14 +0530

    updated notificationStore.js

--- Client/src/lib/notificationStore.js
+++ Client/src/lib/notificationStore.js
@@ -1,8 +1,13 @@
 import { create } from "zustand";
+import apiRequest from "./apiRequest";
 
-const useStore = create((set) => ({
-  bears: 0,
-  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
-  removeAllBears: () => set({ bears: 0 }),
-  updateBears: (newBears) => set({ bears: newBears }),
+const useNotificationStore = create((set) => ({
+  number: 0,
+  fetch: async () => {
+    const res = await apiRequest("/users/notifications");
+    set({ number: res.data });
+  },
+  decrease: () => {
+    set((prev) => ({ number: prev.number - 1 }));
+  },
 }));
