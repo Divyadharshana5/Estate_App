a561c82a1694133bdb5b63b99e6a96ea4367e530
Divyadharshana5 <148339137+Divyadharshana5@users.noreply.github.com>
2025-07-15 11:24:25 +0530

    updated user.controller.js

--- Backend/Controllers/user.controller.js
+++ Backend/Controllers/user.controller.js
@@ -129,3 +129,24 @@ export const profilePosts = async (req, res) => {
     res.status(500).json({ message: "Failed to get profile posts!" });
   }
 };
+
+export const getNotificationNumber = async (req, res) => {
+  const tokenUserId = req.params.id;
+  try {
+    const userPosts = await prisma.post.findMany({
+      where: { userId: tokenUserId },
+    });
+    const saved = await prisma.savedPost.findMany({
+      where: { userId: tokenUserId },
+      include: {
+        post: true,
+      },
+    });
+
+    const savedPost = saved.map((item) => item.post);
+    res.status(200).json({ userPosts, savedPost });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to get profile posts!" });
+  }
+};
